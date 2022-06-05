b87df1653bd274348db53761704e73f9c9e091ab
Aleksandr Leliuk <alexandr.leluk99@gmail.com>
2022-06-05 15:49:46 +0300

    Fix migrations, add logs.

--- flask_project/app/app.py
+++ flask_project/app/app.py
@@ -5,9 +5,17 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 
+from .database import db, migrate
+from .logger import InterceptHandler
+
 app = Flask(__name__)
+
 app.config.from_pyfile('config.py')
+app.logger.addHandler(InterceptHandler())
+
 login_manager = LoginManager(app)
 csrf = CSRFProtect(app)
+db.init_app(app)
+migrate.init_app(app, db)
 
 from . import routs
--- flask_project/app/contoller/user_controller.py
+++ flask_project/app/contoller/user_controller.py
@@ -11,6 +11,7 @@ from werkzeug.security import generate_password_hash
 from ..database.forms.user_form import LoginForm
 from ..database.forms.user_form import RegistrationForm
 from ..database.models.user import User
+from ..logger import logger
 
 
 def profile_user(method, users, profile_id=None):
@@ -37,8 +38,12 @@ def profiles(users, profile_id):
     """
     if users.id == profile_id or users.role.role_name == 'admin':
         user = User.query.filter_by(id=profile_id).first()
+        logger.log("Show", f"<{users.nickname}/{users.role.role_name}>,"
+                           f" show profile user<'{user.nickname}, user if - {profile_id}'>.")
         return {'Nickname': user.nickname, 'Last Name': user.last_name,
                 'First Name': user.first_name, 'Email': user.email, 'Role': user.role.role_name}
+
+    logger.log("Error", f"The user - '{users.nickname}' tried to view information inaccessible to him.")
     return "There's nothing here."
 
 
@@ -77,13 +82,19 @@ def add_user(nickname: str, last_name: str, first_name: str,
     if RegistrationForm(form_input).validate():
         try:
             hash_pwd = generate_password_hash(password)
+
             new_user: object = User(nickname=nickname, last_name=last_name,
                                     first_name=first_name, email=email, password=hash_pwd)
             new_user.save_to_db()
+            logger.log("New_User", f"New user add to db - '{new_user.nickname}'.")
             return f"'{new_user.nickname}' created."
+
         except (IntegrityError, PendingRollbackError):
+            logger.log("Error", f"Registration of user '{new_user.nickname}' failed, user already exists.")
             new_user.rollback()
             return f"user '{new_user.nickname}, {User.query.all()}' already exist."
+
+    logger.log("Error", "An error occurred while registering a new user, incorrect data.")
     return 'Incorrect data'
 
 
@@ -96,10 +107,13 @@ def init_login_user():
     email: str = data["email"]
     password: str = data["password"]
     form_input = ImmutableMultiDict(data)
+
     if email and password and LoginForm(form_input).validate():
         next_page = request.args.get("next")
+
         if not next_page:
             return login_users(email, password)
+
         login_users(email, password)
         return redirect(next_page)
 
@@ -114,7 +128,11 @@ def login_users(email: str, password: str):
     :return:
     """
     user = User.query.filter_by(email=email).first()
+
     if user and check_password_hash(user.password, password):
         login_user(user)
+        logger.log('login_User', f"User logged in '{user.nickname}'")
         return f"Welcome back, '{user}'"
+
+    logger.log("Error", f"An attempt was made to log in to a user account - '{user.nickname if user else None}'.")
     return 'bad password.'  # flash('bad password')
